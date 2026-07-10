831779958cc1f8806c132c6f76e59f63d63f8b1d
JATINKARTHIK-VADLAMUDI <jatinkarthik2006@gmail.com>
2026-07-10 13:03:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -369,6 +369,46 @@ app.put("/api/patients/:id", async (req, res) => {
 
 });
 
+// ---------------- DELETE PATIENT ----------------
+
+app.delete("/api/patients/:id", async (req,res)=>{
+
+    try{
+
+        const { ObjectId } = require("mongodb");
+
+        await patientsCollection.deleteOne({
+
+            _id:new ObjectId(req.params.id)
+
+        });
+
+        res.json({
+
+            success:true,
+
+            message:"Patient Deleted Successfully"
+
+        });
+
+    }
+
+    catch(err){
+
+        console.log(err);
+
+        res.status(500).json({
+
+            success:false,
+
+            message:"Unable to Delete Patient"
+
+        });
+
+    }
+
+});
+
 // ---------------- SERVER ----------------
 
 const PORT = process.env.PORT || 3000;
